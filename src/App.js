c82a628883aba17da1f0a1bef0727882346b49fd
CatherineHonors <cathonors@gmail.com>
2023-01-05 17:12:48 -0500

    started on project, need to add photos and more styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
-
+import br from "./images/br.jpg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header-title">
+        <h1 className="header">Gimme Vegan Food Please</h1>
+      </div>
+      <div className="App">
+        <img src={br} alt="brooke" className="brookeimg" />
+        <section className="bio">
+          My name is <b>Brooke</b> and I have been vegan for 5️⃣ years!
+          <br></br>
+          <br></br>
+          ⭐️When I'm not checking out the newest vegan places I work as mental
+          health therapist for kids and teens. I work with anxiety, depression,
+          self harm, ADHD, self esteem, and so much more! My work is very
+          rewarding but can be overwhelming at times so I love to use this page
+          as my creative outlet purely because I love my instagram community!
+          <br></br>
+          <br></br>
+          ⭐️I am currently engaged to the love of my life and planning my
+          wedding! I will share closer to the vegan dinner option and desert I
+          have chosen but for now I'm keeping it a surprise!
+          <br></br>
+          <br />
+          ⭐️ I have two dogs, one shitzu named Benny and Brussels Griffon named
+          Romy. I also have a Brussels Griffon niece named Cooper and my fiancés
+          dads dog Cavapoo named Emmy! These four dogs are my loves and I am so
+          grateful to have them in my life!
+          <br></br>
+          <br />
+          ⭐️Going vegan is my favorite thing I've ever done and I hope to show
+          others that eating vegan doesn't have to be boring.
+          <br></br>
+          <br />
+          Check out my instagram here and my facebook here. For business
+          ventures, please contact me at email. Amazon, TikTok page
+        </section>
+        <div className="grid-container">
+          <img
+            src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg"
+            alt="food photography"
+            className="grid-item"
+          />
+          <img
+            src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg"
+            alt="food photography"
+            className="grid-item"
+          />
+          <img
+            src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg"
+            alt="food photography"
+            className="grid-item"
+          />
+          <img
+            src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg"
+            alt="food photography"
+            className="grid-item"
+          />
+          <img
+            src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg"
+            alt="food photography"
+            className="grid-item"
+          />
+          <img
+            src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg"
+            alt="food photography"
+            className="grid-item"
+          />
+          <img
+            src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg"
+            alt="food photography"
+            className="grid-item"
+          />
+          <img
+            src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg"
+            alt="food photography"
+            className="grid-item"
+          />
+        </div>
+        <div className="footer">
+          <p>
+            <a href="https://www.instagram.com/gimmeveganfoodplease/">INSTA</a>|
+            <a href="https://www.facebook.com/profile.php?id=100063769185171">
+              FACEBOOK
+            </a>
+            |
+            <a href="https://www.amazon.com/shop/gimmeveganfoodplease">
+              AMAZON
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
 
